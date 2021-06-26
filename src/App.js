b6315a0a9400315dc5c2d5e95c55368824dcf8d9
ccchippp <clempke90@gmail.com>
2021-06-26 15:41:21 -0400

    added counter with current state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import './App.css';
 
-const useState = React
+const {useState} = React
 
 export default function App() {
+  const [counter, setCounter] = useState(0)
     return (
         <div className="App">
             <h1>Interview Practice</h1>
+            <p>
+              {counter}
+            </p>
+            <button> + 1 </button>
         </div>
     )
 
