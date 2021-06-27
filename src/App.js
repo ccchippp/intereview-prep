3364ad5299e6ca825bd73f4389c1e1f97b24f429
ccchippp <clempke90@gmail.com>
2021-06-27 15:38:25 -0400

    fecthing data

--- src/App.js
+++ src/App.js
@@ -2,41 +2,49 @@ import * as React from 'react';
 import './App.css';
 import axios from 'axios'
 
-const {useState} = React
+const {useEffect, useState} = React
 
 
 export default function App() {
   const [counter, setCounter] = useState(0)
-    return (
-        <div className="App">
-            <h1>Interview Practice</h1>
-            <p>
-              {counter}
-            </p>
-            <button
-              onClick={() => setCounter(counter + 1)}
-            > + 1 </button>
-            <button
-              onClick={() => setCounter(counter - 1)}
-            > - 1 </button>
-            <button
-              onClick={() => {
-                fetchRandomData()}}
-            > Fetch Random User </button>
+  const [randomUserDataJSON, setRandomUserDataJSON] = useState('')
+
+  useEffect(() => {
+    const randomData = await fetchRandomData()
+    setRandomUserDataJSON(randomData)
+  }, [])
+    
+  return (
+    <div className="App">
+        <h1>Interview Practice</h1>
+        <p>
+          {counter}
+        </p>
+        <button
+          onClick={() => setCounter(counter + 1)}
+        > + 1 </button>
+        <button
+          onClick={() => setCounter(counter - 1)}
+        > - 1 </button>
+        {/* <button
+          onClick={() => {
+            fetchRandomData()}}
+        > Fetch Random User </button> */}
+        <div className="random_user_data">
+
         </div>
-    )
+    </div>
+  )
 
 }
 
 // would normally be in an api file
-
 const fetchRandomData = () => {
-
   return axios.get('https://randomuser.me/api')
-  .then(res => {
+  .then(({data}) => {
     // handle success
-    console.log(res);
-    return res
+    console.log(data);
+    return JSON.stringify(data)
   })
   .catch(err => {
     // handle error
