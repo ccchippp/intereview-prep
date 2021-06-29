adfc776b06ed9679687d80d6f73676a3bd94ce69
ccchippp <clempke90@gmail.com>
2021-06-29 09:51:25 -0400

    fetch with picture

--- src/App.js
+++ src/App.js
@@ -4,15 +4,38 @@ import axios from 'axios'
 
 const {useEffect, useState} = React
 
+const getFullUserName = (userInfo) => {
+  const {name: {first, last}} = userInfo
+  return `${first} ${last}`
+}
+
+const getUserPicture = (userInfo) => {
+
+}
+
+// would normally be in an api file
+const fetchRandomData = () => {
+  return axios
+  .get('https://randomuser.me/api')
+  .then(({ data }) => {
+    // handle success
+    console.log(data);
+    return data
+  })
+  .catch(err => {
+    // handle error
+    console.log(err);
+  })
+}
 
 export default function App() {
   const [counter, setCounter] = useState(0)
-  const [userInfos, setUserInfos] =useState([])
+  const [userInfos, setUserInfos] = useState([])
   const [randomUserDataJSON, setRandomUserDataJSON] = useState('')
 
   useEffect(() => {
     fetchRandomData().then(randomData => {
-      setRandomUserDataJSON(randomData || 'No user data found')
+      setRandomUserDataJSON(JSON.stringify(randomData, null, 2) || 'No user data found')
       setUserInfos(randomData.results)
     })
   }, [])
@@ -29,26 +52,20 @@ export default function App() {
         <button
           onClick={() => setCounter(counter - 1)}
         > - 1 </button>
-        <div className="random_user_data">
+        {/* <div className="random_user_data">
           <pre>
             {randomUserDataJSON}
           </pre>
-        </div>
+        </div> */}
+        {
+          userInfos.map((userInfo, idx) => (
+            <div key={idx}>
+            <img src={userInfo.picture.thumbnail} alt=""/>
+            <p>{getFullUserName(userInfo)}</p>
+            </div>
+          ))
+        }
     </div>
   )
 
-}
-
-// would normally be in an api file
-const fetchRandomData = async () => {
-  return axios.get('https://randomuser.me/api')
-  .then(({data}) => {
-    // handle success
-    console.log(data);
-    return JSON.stringify(data, null, 2)
-  })
-  .catch(err => {
-    // handle error
-    console.log(err);
-  })
 }
