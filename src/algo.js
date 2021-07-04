17d597dbc858eea45e7913367590102c336668c7
ccchippp <clempke90@gmail.com>
2021-07-04 12:45:09 -0400

    123 number flip algo prac

--- src/algo.js
+++ src/algo.js
@@ -1,6 +1,33 @@
 import React from 'react'
 
 export default function algo() {
+        let n = 3122
+        function solve(n) {
+    
+            let num = n.toString()
+            let numArray = num.split('')
+    
+            // for (let i = 0; i < num.length; i++) {
+            for (let i = 0; numArray[i] = !3; i++) {
+                numArray[i] = 3
+                let output = numArray.join('')
+                let int = parseInt(output)
+                return int
+            }
+                // if (numArray[i] != 3) {
+                //     numArray[i] = 3
+               // let output = numArray.join('')
+                // let int = parseInt(output)
+                // return int 
+                // } 
+                // else {
+                // return n
+                // }
+            // }
+            let output = numArray.join('')
+            let int = parseInt(output)
+            return int
+        }
     return (
         <div>
             
