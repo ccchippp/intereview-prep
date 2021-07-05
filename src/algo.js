800c3afd0537d4ddcc93b89f7d1151cce446c95c
ccchippp <clempke90@gmail.com>
2021-07-05 17:51:34 -0400

    cleaned

--- src/algo.js
+++ src/algo.js
@@ -14,16 +14,6 @@ export default function algo() {
                 let int = parseInt(output)
                 return int
             }
-                // if (numArray[i] != 3) {
-                //     numArray[i] = 3
-               // let output = numArray.join('')
-                // let int = parseInt(output)
-                // return int 
-                // } 
-                // else {
-                // return n
-                // }
-            // }
             let output = numArray.join('')
             let int = parseInt(output)
             return int
