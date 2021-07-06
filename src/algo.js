1fcc1f31f3b5d92e954ee5f662feea7e22db6946
ccchippp <clempke90@gmail.com>
2021-07-06 19:00:16 -0400

    123 Flip COMPLETE

--- src/algo.js
+++ src/algo.js
@@ -1,19 +1,24 @@
 import React from 'react'
 
 export default function algo() {
-        let n = 3122
-        function solve(n) {
-    
-            let num = n.toString()
-            let numArray = num.split('')
-    
-            // for (let i = 0; i < num.length; i++) {
-            for (let i = 0; numArray[i] = !3; i++) {
-                numArray[i] = 3
-            }
+    let solve = (n) => {
+        let num = n.toString()
+        let numArray = num.split('')
+
+        for (let i = 0; i < numArray.length;) {
+            if (numArray[i] == 3) {
+                i++
+            } else if (numArray[i] != 3) {
+            numArray[i] = 3
             let output = numArray.join('')
             let int = parseInt(output)
             return int
+            } else {
+                return n
+            }
+        }
+        return n
+    }
         }
     return (
         <div>
